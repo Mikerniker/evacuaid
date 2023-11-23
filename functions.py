df9d0f1f773c316b72598909aa6bbe5b5ad6fd04
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2023-11-24 04:53:01 +0800

    Fixed errors in situation report added option to save and print variables.

--- functions.py
+++ functions.py
@@ -4,12 +4,12 @@ def get_reports(filepath=FILEPATH):
     """ Read a csv file and return the list of
     reports.
     """
-    with open(filepath, "r") as file_local:
+    with open(filepath, "r", encoding="utf-8") as file_local:
         reports_local = file_local.readlines()
     return reports_local
 
 
 def write_reports(reports_arg, filepath=FILEPATH):
     """Write report in the csv file"""
-    with open(filepath, 'w') as file:
+    with open(filepath, 'w', encoding="utf-8") as file:
         file.writelines(reports_arg)
--- pages/2_📝_Situation_Report.py
+++ pages/2_📝_Situation_Report.py
@@ -6,17 +6,16 @@ st.header('Situation Report')
 
 # st.set_page_config(layout="wide")
 
-
-all_reports = functions.get_reports()  # to modify
+all_reports = functions.get_reports()
 
 @st.cache_data()
-def add_report(date, time, situation, population, response):
-    # report = st.session_state[] + "\n"
-    # print(report)
-    report = date, time, situation, population, response + "\n"
+def add_report(date, time, situation, population, displaced, response):
+    report = f"{date}, {time}, {situation}, " \
+             f"{population}, {displaced}, {response}\n"
     all_reports.append(report)
     functions.write_reports(all_reports)
-    return date, time, situation, population, response
+    return report
+
 
 date = st.date_input("Add date", value=None)
 
@@ -35,7 +34,17 @@ st.write('IV. Response Actions and Interventions')
 response = st.text_input('Describe standby funds, food and nonfood items, other activities')
 
 if st.button("Save report"):
-    add_report(date, time, situation, affected_population, response)
-    st.write(date, time, situation, affected_population, response)
+    add_report(date, time, situation, affected_population, displaced, response)
+    # st.write(date, time, situation, affected_population, response)
+    st.write(f"Date: {date}")
+    st.write(f"Time: {time}")
+    st.write('I. Situation Overview')
+    st.write(f"Situation: {situation}")
+    st.write('II. Status of Affected Areas and Population')
+    st.write(f"Affected Population: {affected_population}")
+    st.write('III. Status of Displaced Population')
+    st.write(f"Response: {displaced}")
+    st.write('IV. Response Actions and Interventions')
+    st.write(f"Response: {response}")
 
 # st.write(pd.DataFrame(add_report()))
