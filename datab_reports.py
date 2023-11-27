4c4f98267dd423c6f82c1bcb6bcfa2d623bb8b36
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2023-11-28 02:28:42 +0800

    modified situation report and datab_reports by fixing the table in st.data_editor and fixing the db relationship in datab_reports

--- datab_reports.py
+++ datab_reports.py
@@ -1,6 +1,6 @@
-from sqlalchemy import Column, Integer, String, create_engine, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, Enum, ForeignKey, create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
 
 Base = declarative_base()
 
@@ -17,21 +17,47 @@ class Reports(Base):
     response = Column(String(500), nullable=False)
     preparer = Column(String(250), nullable=False)
     releaser = Column(String(250), nullable=False)
+    inventory = relationship('Inventory', back_populates='reports')
+
+class Inventory(Base):
+    __tablename__ = 'inventory'
+
+    class ItemEnum:
+        rice = "rice"
+        flour = "flour"
+        water = "water"
+        milk = "milk"
+        sugar ="sugar"
+        canned_goods = "canned goods"
+        cooking_oil = "cooking oil"
+        blankets = "blankets"
+        clothing = "clothing"
+        tents = "tents"
+        hygiene = "hygiene kits"
+
+    id = Column(Integer, primary_key=True)
+    item = Column(String(250), nullable=False, default=ItemEnum.rice)
+    quantity = Column(Integer, nullable=False)
+    is_available = Column(Boolean, nullable=False)
+    evacuation_site = Column(String(250),
+                             ForeignKey('all_reports.evacuation_site'),
+                             nullable=False)
+    reports = relationship('Reports', back_populates='inventory')
     # item = Column(String(250), nullable=False)
     # quantity = Column(Integer(250), nullable=False)
     # is_available = Column(Boolean, nullable=False),
     # inventory = Column(String(250), nullable=False)
-    rice = Column(Integer, nullable=False)
-    flour = Column(Integer, nullable=False)
-    sugar = Column(Integer, nullable=False)
-    powdered_milk = Column(Integer, nullable=False)
-    canned_goods = Column(Integer, nullable=False)
-    cooking_oil = Column(Integer, nullable=False)
-    blankets = Column(Integer, nullable=False)
-    clothing = Column(Integer, nullable=False)
-    tents = Column(Integer, nullable=False)
-    water = Column(Integer, nullable=False)
-    hygiene_kits = Column(Integer, nullable=False)
+    # rice = Column(Integer, nullable=False)
+    # flour = Column(Integer, nullable=False)
+    # sugar = Column(Integer, nullable=False)
+    # powdered_milk = Column(Integer, nullable=False)
+    # canned_goods = Column(Integer, nullable=False)
+    # cooking_oil = Column(Integer, nullable=False)
+    # blankets = Column(Integer, nullable=False)
+    # clothing = Column(Integer, nullable=False)
+    # tents = Column(Integer, nullable=False)
+    # water = Column(Integer, nullable=False)
+    # hygiene_kits = Column(Integer, nullable=False)
 
 
 engine = create_engine('sqlite:///reports.db')
--- pages/2_📝_Situation_Report.py
+++ pages/2_📝_Situation_Report.py
@@ -4,101 +4,126 @@ import functions
 from datab_reports import Reports, Inventory, Session, Base
 from password_check import check_password
 
-if check_password():
-    st.header('Situation Report')
+# if check_password():
+st.header('Situation Report')
 
-    all_reports = functions.get_reports()
+all_reports = functions.get_reports()
 
-    @st.cache_data()
-    def add_report(evac_site, date, time, situation, population,
-                   displaced, response, preparer, releaser, inventory_data):
+@st.cache_data()
+def add_report(evac_site, date, time, situation, population,
+               displaced, response, preparer, releaser, inventory_data):
 
-        session = Session()
+    session = Session()
 
-        date_str = str(date)
-        time_str = str(time)
+    date_str = str(date)
+    time_str = str(time)
 
-        new_report = Reports(evacuation_site=evac_site, date=date_str,
-                             time=time_str, situation=situation,
-                             affected_pop=population, displaced=displaced,
-                             response=response, preparer=preparer,
-                             releaser=releaser)
+    new_report = Reports(evacuation_site=evac_site, date=date_str,
+                         time=time_str, situation=situation,
+                         affected_pop=population, displaced=displaced,
+                         response=response, preparer=preparer,
+                         releaser=releaser)
 
-        # Link the new Reports entry to Inventory entries
-        for item_data in inventory_data:
-            is_available = item_data['is_available'].lower() == 'true'
-            item = Inventory(
-                item=item_data['item'],
-                quantity=item_data['quantity'],
-                is_available=is_available
-            )
-            new_report.inventory.append(item)
-
-
-        session.add(new_report)
-        session.commit()
-        session.close()
-
-    df = pd.read_csv('marikina_evacuation_centers.csv', usecols=['CENTER_M'])
-    evac_sites_list = list(df["CENTER_M"])
-
-    evacuation_site = st.selectbox('Select Evacuation Site', evac_sites_list)
-
-    date = st.date_input("Add date", value=None)
-
-    time = st.time_input('Add time', value=None)
-
-    situation = st.text_input('Give an overview of the situation')
-
-    affected_population = st.text_input('Describe the status of affected areas and population')
-
-    displaced = st.text_input('Describe the status of displaced population')
-
-    response = st.text_input('Describe reponse actions and interventions, i.e.'
-                             ' standby funds, food and nonfood items, other activities')
-
-    preparer = st.text_input('Prepared by')
-
-    releaser = st.text_input('Released by')
-
-    # # Table
-    # Initialize empty DataFrame for the Inventory data
-    inventory_data = pd.DataFrame(columns=["item", "quantity", "is_available"])
-
-    # Display Inventory table using st.data_editor
-    edited_inventory_df = st.data_editor(
-        inventory_data,
-        column_config={"inventory": st.column_config.ProgressColumn("inventory",
-                                                                    help="Volume in tons",
-                                                                    min_value=0,
-                                                                    max_value=100),},
-        num_rows="dynamic")
-
-    st.write('Inventory List')
+    # Link the new Reports entry to Inventory entries
+    for index, item_data in inventory_data.iterrows():
+        # is_available = item_data['is_available']
+        item = Inventory(
+            item=item_data['item'],
+            quantity=item_data['quantity'],
+            is_available=item_data['is_available']
+        )
+        new_report.inventory.append(item)
 
 
-    on = st.toggle('Activate feature')
+    session.add(new_report)
+    session.commit()
+    session.close()
 
-    if on:
-        st.write('Feature activated!')
+df = pd.read_csv('marikina_evacuation_centers.csv', usecols=['CENTER_M'])
+evac_sites_list = list(df["CENTER_M"])
 
-    if st.button("Save report"):
+evacuation_site = st.selectbox('Select Evacuation Site', evac_sites_list)
 
-        # Retrieve user-modified Inventory data
-        edited_inventory_data = edited_inventory_df.to_dict(orient="records")
+date = st.date_input("Add date", value=None)
 
-        add_report(evacuation_site, date, time, situation, affected_population,
-                   displaced, response, preparer, releaser, edited_inventory_data)
-        # Convert the edited_df to SQLite and save it
-        conn = st.connection("local_db", type="sql", url="sqlite:///reports.db")
-        # cursor = conn.cursor()
-        Session().commit()
+time = st.time_input('Add time', value=None)
 
-        # Close the session
-        Session().close()
+situation = st.text_input('Give an overview of the situation')
+
+affected_population = st.text_input('Describe the status of affected areas and population')
+
+displaced = st.text_input('Describe the status of displaced population')
+
+response = st.text_input('Describe reponse actions and interventions, i.e.'
+                         ' standby funds, food and nonfood items, other activities')
+
+preparer = st.text_input('Prepared by')
+
+releaser = st.text_input('Released by')
+
+# # Table
+
+# Define the possible values for the ItemEnum
+item_enum_values = [Inventory.ItemEnum.rice, Inventory.ItemEnum.flour, Inventory.ItemEnum.water,
+                    Inventory.ItemEnum.milk, Inventory.ItemEnum.sugar, Inventory.ItemEnum.canned_goods,
+                    Inventory.ItemEnum.cooking_oil, Inventory.ItemEnum.blankets, Inventory.ItemEnum.clothing,
+                    Inventory.ItemEnum.tents, Inventory.ItemEnum.hygiene]
 
 
-        st.success("Report added successfully!")
+# Initialize empty DataFrame for the Inventory data
+item_enum_values = [getattr(Inventory.ItemEnum, attr) for attr in dir(Inventory.ItemEnum) if not callable(getattr(Inventory.ItemEnum, attr)) and not attr.startswith("__")]
+
+
+
+inventory_data = pd.DataFrame(columns=["item", "quantity", "is_available", "inventory"])
+inventory_data["item"] = item_enum_values
+inventory_data["quantity"] = 0
+inventory_data["is_available"] = False
+inventory_data["inventory"] = inventory_data['quantity']
+
+# Display Inventory table using st.data_editor
+edited_inventory_df = st.data_editor(
+    inventory_data,
+    column_config={"item": st.column_config.TextColumn("item", help="Item"),
+                   "quantity": st.column_config.NumberColumn("quantity",
+                                                             help="in tons",
+                                                             min_value=0,
+                                                             max_value=100),
+                   "is_available": st.column_config.CheckboxColumn("is_available",
+                                                                   help="Is Available"),
+                   "inventory": st.column_config.ProgressColumn("inventory",
+                                                                help="Volume in tons",
+                                                                min_value=0,
+                                                                max_value=100)},
+    num_rows="dynamic")
+
+st.write('Inventory List')
+
+
+on = st.toggle('Activate feature')
+
+if on:
+    st.write('Feature activated!')
+
+if st.button("Save report"):
+
+    # edited_inventory_data = edited_inventory_df.to_dict(orient="records")
+    # Retrieve user-modified Inventory data
+    edited_inventory_data = edited_inventory_df.copy()
+
+    add_report(evacuation_site, date, time, situation, affected_population,
+               displaced, response, preparer, releaser, edited_inventory_data)
+
+    # Convert the edited_df to SQLite and save it
+    conn = st.connection("local_db", type="sql", url="sqlite:///reports.db")
+
+    Session().commit()
+
+    # Close the session
+    Session().close()
+
+
+    st.success("Report added successfully!")
 
 
 
