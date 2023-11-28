7422c98d1c45b845632f56a544bb4149c6428b51
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2023-11-29 03:30:29 +0800

    fixed check password in situation report

--- pages/Chat.py
+++ pages/Chat.py
@@ -3,25 +3,29 @@ import numpy as np
 import random
 import time
 from openai import OpenAI
+import pandas as pd
+
+
+
+df = pd.read_csv('marikina_evacuation_centers.csv')
 
 st.title("EvacuAid Assistant")
-
 
 # Set OpenAI API key from Streamlit secrets
 client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
 # Set a default model
-if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-3.5-turbo"
+# if "openai_model" not in st.session_state:
+#     st.session_state["openai_model"] = "gpt-3.5-turbo"
 
 # Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-# Display chat messages from history on app rerun
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
+
 
 # Accept user input
 if prompt := st.chat_input("How can I help you?"):
@@ -30,20 +34,44 @@ if prompt := st.chat_input("How can I help you?"):
     # Display user message in chat message container
     with st.chat_message("user"):
         st.markdown(prompt)
-    # Display assistant response in chat message container
-    with st.chat_message("assistant"):
-        message_placeholder = st.empty()
-        full_response = ""
+    # # Display assistant response in chat message container
+    # with st.chat_message("assistant"):
+    #     message_placeholder = st.empty()
+    #     full_response = ""
 
-for response in client.chat.completions.create(
-        model=st.session_state["openai_model"],
-        messages=[{"role": m["role"], "content": m["content"]} for m in st.session_state.messages],
-        stream=True,):
-    full_response += (response.choices[0].delta.content or "")
-    message_placeholder.markdown(full_response + "▌")
-    message_placeholder.markdown(full_response)
+    # Process user query and extract information from the CSV file
+    response_text = "I'm sorry, I couldn't find the information you requested."
 
-st.session_state.messages.append({"role": "assistant", "content": full_response})
+    if "address" in prompt.lower():
+        site_name = prompt.split("address of")[1].strip()
+        site_info = df[df['CENTER_M'] == site_name, 'LOCATION']
+        if not site_info.empty:
+            response_text = f"The address of {site_name} is {site_info.LOCATION[0]}."
+
+        elif "contact information" in prompt.lower() or "contact info" in prompt.lower():
+            site_name = prompt.split("contact information of")[1].strip()
+            site_info = df[df['CENTER_M'] == site_name]
+            if not site_info.empty:
+                response_text = f"The contact information of {site_name} is {site_info.CONTACT_NUMBER[0]}."
+
+        # Display assistant response in chat message container
+        with st.chat_message("assistant"):
+            st.markdown(response_text)
+
+        # Add assistant response to chat history
+        st.session_state.messages.append({"role": "assistant", "content": response_text})
+
+
+
+# for response in client.chat.completions.create(
+#         model=st.session_state["openai_model"],
+#         messages=[{"role": m["role"], "content": m["content"]} for m in st.session_state.messages],
+#         stream=True,):
+#     full_response += (response.choices[0].delta.content or "")
+#     message_placeholder.markdown(full_response + "▌")
+#     message_placeholder.markdown(full_response)
+#
+# st.session_state.messages.append({"role": "assistant", "content": full_response})
 
 # ORIGINAL
 # Initialize chat history
--- pages/Situation_Report.py
+++ pages/Situation_Report.py
@@ -11,229 +11,111 @@ st.set_page_config(
     layout="wide",
 )
 
-# if check_password():
-st.header('Situation Report')
+if check_password():
+    st.header('Situation Report')
 
-all_reports = functions.get_reports()
+    all_reports = functions.get_reports()
 
-@st.cache_data()
-def add_report(evac_site, date, time, situation, population,
-               displaced, response, preparer, releaser, activate, inventory_data):
+    @st.cache_data()
+    def add_report(evac_site, date, time, situation, population,
+                   displaced, response, preparer, releaser, activate, inventory_data):
 
-    session = Session()
+        session = Session()
 
-    date_str = str(date)
-    time_str = str(time)
+        date_str = str(date)
+        time_str = str(time)
 
-    new_report = Reports(evacuation_site=evac_site, date=date_str,
-                         time=time_str, situation=situation,
-                         affected_pop=population, displaced=displaced,
-                         response=response, preparer=preparer,
-                         releaser=releaser, activate=activate)
+        new_report = Reports(evacuation_site=evac_site, date=date_str,
+                             time=time_str, situation=situation,
+                             affected_pop=population, displaced=displaced,
+                             response=response, preparer=preparer,
+                             releaser=releaser, activate=activate)
 
-    # Link the new Reports entry to Inventory entries
-    for index, item_data in inventory_data.iterrows():
-        # is_available = item_data['is_available']
-        item = Inventory(
-            item=item_data['item'],
-            quantity=item_data['quantity'],
-            is_available=item_data['is_available']
-        )
-        new_report.inventory.append(item)
-
-
-    session.add(new_report)
-    session.commit()
-    session.close()
+        # Link the new Reports entry to Inventory entries
+        for index, item_data in inventory_data.iterrows():
+            # is_available = item_data['is_available']
+            item = Inventory(
+                item=item_data['item'],
+                quantity=item_data['quantity'],
+                is_available=item_data['is_available']
+            )
+            new_report.inventory.append(item)
 
 
-df = pd.read_csv('marikina_evacuation_centers.csv', usecols=['CENTER_M'])
-evac_sites_list = [''] + list(df["CENTER_M"])
-evacuation_site = st.selectbox('Select Evacuation Site', evac_sites_list)
-
-date = st.date_input("Add date", value=None)
-
-time = st.time_input('Add time', value=None)
-
-situation = st.text_input('Give an overview of the situation')
-
-affected_population = st.text_input('Describe the status of affected areas and population')
-
-displaced = st.text_input('Describe the status of displaced population')
-
-response = st.text_input('Describe reponse actions and interventions, i.e.'
-                         ' standby funds, food and nonfood items, other activities')
-
-preparer = st.text_input('Prepared by')
-
-releaser = st.text_input('Released by')
-
-# # Table
-
-# Initialize empty DataFrame for the Inventory data
-item_enum_values = [getattr(Inventory.ItemEnum, attr) for attr in dir(Inventory.ItemEnum) if not callable(getattr(Inventory.ItemEnum, attr)) and not attr.startswith("__")]
-
-inventory_data = pd.DataFrame(columns=["item", "quantity", "is_available", "inventory"])
-inventory_data["item"] = item_enum_values
-inventory_data["quantity"] = 0
-inventory_data["is_available"] = False
-inventory_data["inventory"] = inventory_data['quantity']
-
-# Display Inventory table using st.data_editor
-edited_inventory_df = st.data_editor(
-    inventory_data,
-    column_config={"item": st.column_config.TextColumn("item", help="Item"),
-                   "quantity": st.column_config.NumberColumn("quantity",
-                                                             help="in tons",
-                                                             min_value=0,
-                                                             max_value=100),
-                   "is_available": st.column_config.CheckboxColumn("is_available",
-                                                                   help="Is Available"),
-                   "inventory": st.column_config.ProgressColumn("inventory",
-                                                                help="Volume in tons",
-                                                                min_value=0,
-                                                                max_value=100)},
-    num_rows="dynamic",
-    use_container_width=True,
-    hide_index=True)
-
-# st.write('Inventory List')
+        session.add(new_report)
+        session.commit()
+        session.close()
 
 
-activate_aid = st.toggle('Activate Aid Request')
+    df = pd.read_csv('marikina_evacuation_centers.csv', usecols=['CENTER_M'])
+    evac_sites_list = [''] + list(df["CENTER_M"])
+    evacuation_site = st.selectbox('Select Evacuation Site', evac_sites_list)
 
-# if on:
-#     st.write('Feature activated!')
+    date = st.date_input("Add date", value=None)
 
-if st.button("Save report"):
+    time = st.time_input('Add time', value=None)
 
-    # edited_inventory_data = edited_inventory_df.to_dict(orient="records")
-    # Retrieve user-modified Inventory data
-    edited_inventory_data = edited_inventory_df.copy()
+    situation = st.text_input('Give an overview of the situation')
 
-    add_report(evacuation_site, date, time, situation, affected_population,
-               displaced, response, preparer, releaser, activate_aid, edited_inventory_data)
+    affected_population = st.text_input('Describe the status of affected areas and population')
 
-    # Convert the edited_df to SQLite and save it
-    conn = st.connection("local_db", type="sql", url="sqlite:///reports.db")
+    displaced = st.text_input('Describe the status of displaced population')
 
-    Session().commit()
+    response = st.text_input('Describe reponse actions and interventions, i.e.'
+                             ' standby funds, food and nonfood items, other activities')
 
-    # Close the session
-    Session().close()
+    preparer = st.text_input('Prepared by')
 
+    releaser = st.text_input('Released by')
 
-    st.success("Report added successfully!")
+    # # Table
 
+    # Initialize empty DataFrame for the Inventory data
+    item_enum_values = [getattr(Inventory.ItemEnum, attr) for attr in dir(Inventory.ItemEnum) if not callable(getattr(Inventory.ItemEnum, attr)) and not attr.startswith("__")]
 
+    inventory_data = pd.DataFrame(columns=["item", "quantity", "is_available", "inventory"])
+    inventory_data["item"] = item_enum_values
+    inventory_data["quantity"] = 0
+    inventory_data["is_available"] = False
+    inventory_data["inventory"] = inventory_data['quantity']
 
-
- #DELETED
-# # Create the table if it doesn't exist
-# cursor.execute("""
-#     CREATE TABLE IF NOT EXISTS all_reports (
-#         evacuation_site TEXT,
-#         date TEXT,
-#         time TEXT,
-#         situation TEXT,
-#         affected_pop TEXT,
-#         displaced TEXT,
-#         response TEXT,
-#         preparer TEXT,
-#         releaser TEXT,
-#         item TEXT,
-#         quantity INTEGER,
-#         is_available INTEGER,
-#         inventory INTEGER
-#     )
-# """)
-#
-# # Clear existing data from the table
-# cursor.execute("DELETE FROM all_reports")
-#
-# # Insert the values into the table
-# for index, row in edited_df.iterrows():
-#     cursor.execute("""
-#         INSERT INTO all_reports (
-#             evacuation_site, date, time, situation, affected_pop,
-#             displaced, response, preparer, releaser, item, quantity,
-#             is_available, inventory
-#         ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-#     """, (
-#         evacuation_site, date, time, situation, affected_population,
-#         displaced, response, preparer, releaser, row['item'],
-#         row['quantity'], row['is_available'], row['inventory']
-#     ))
+    # Display Inventory table using st.data_editor
+    edited_inventory_df = st.data_editor(
+        inventory_data,
+        column_config={"item": st.column_config.TextColumn("item", help="Item"),
+                       "quantity": st.column_config.NumberColumn("quantity",
+                                                                 help="in tons",
+                                                                 min_value=0,
+                                                                 max_value=100),
+                       "is_available": st.column_config.CheckboxColumn("is_available",
+                                                                       help="Is Available"),
+                       "inventory": st.column_config.ProgressColumn("inventory",
+                                                                    help="Volume in tons",
+                                                                    min_value=0,
+                                                                    max_value=100)},
+        num_rows="dynamic",
+        use_container_width=True,
+        hide_index=True)
 
 
-# edited_df = st.data_editor(
-#     initial_data,
-#     column_config={
-#         "inventory": st.column_config.ProgressColumn(
-#             "Over or Under",
-#             help="Volume in tons",
-#             min_value=0,
-#             max_value=100,
-#         ),
-#     }, num_rows="dynamic")
+    activate_aid = st.toggle('Activate Aid Request')
 
 
-# edited_df = st.data_editor(df, num_rows="dynamic")
-# edited_df = st.data_editor(
-#     initial_data,
-#     column_config={
-#         "inventory": st.column_config.ProgressColumn(
-#             "Over or Under",
-#             help="Volume in tons",
-#             min_value=0,
-#             max_value=100,
-#         ),
-#     }, num_rows="dynamic")
+    if st.button("Save report"):
+        # Retrieve user-modified Inventory data
+        edited_inventory_data = edited_inventory_df.copy()
 
-# initial_data = pd.DataFrame(
-#     [
-#         {"item": "Rice", "quantity": 50, "is_available": True, "inventory": 0},
-#         {"item": "Flour", "quantity": 25, "is_available": False, "inventory": 0},
-#         {"item": "Sugar", "quantity": 70, "is_available": True, "inventory": 0},
-#         {"item": "Powdered Milk", "quantity": 120, "is_available": True, "inventory": 0},
-#         {"item": "Canned Goods", "quantity": 65, "is_available": True, "inventory": 0},
-#         {"item": "Cooking Oil", "quantity": 70, "is_available": True, "inventory": 0},
-#         {"item": "Blankets", "quantity": 54, "is_available": True, "inventory": 0},
-#         {"item": "Clothing", "quantity": 140, "is_available": True, "inventory": 0},
-#         {"item": "Tents", "quantity": 15, "is_available": True, "inventory": 0},
-#         {"item": "Water", "quantity": 45, "is_available": True, "inventory": 0},
-#         {"item": "Hygiene Kits", "quantity": 30, "is_available": True, "inventory": 0},
-#     ]
-# )
-# conn = st.connection('reports', type='sqlite')
+        add_report(evacuation_site, date, time, situation, affected_population,
+                   displaced, response, preparer, releaser, activate_aid, edited_inventory_data)
+
+        # Convert the edited_df to SQLite and save it
+        conn = st.connection("local_db", type="sql", url="sqlite:///reports.db")
+
+        Session().commit()
+
+        # Close the session
+        Session().close()
 
 
-#
+        st.success("Report added successfully!")
 
-
-    # st.write(date, time, situation, affected_population, response)
-
-
-# Display the updated data
-# st.dataframe(edited_df)
-
-# session = Session()
-# reports = session.query(Reports).all()
-#
-# for report in reports:
-#     st.write(f"Evacuation Site: {report.evacuation_site}")
-#     st.write(f"Date: {report.date}")
-#     st.write(f"Time: {report.time}")
-#     st.write('I. Situation Overview')
-#     st.write(f"{report.situation}")
-#     st.write('II. Status of Affected Areas and Population')
-#     st.write(f"{report.affected_pop}")
-#     st.write('III. Status of Displaced Population')
-#     st.write(f"{report.displaced}")
-#     st.write('IV. Response Actions and Interventions')
-#     st.write(f"{report.response}")
-#     st.write(f"Prepared by: {report.preparer}")
-#     st.write(f"Released by: {report.releaser}")
-#
-# session.close()
-# st.write(pd.DataFrame(add_report()))
