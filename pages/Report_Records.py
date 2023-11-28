276124af0de804b98def549ce74cfcef5ca95e12
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2023-11-29 01:19:02 +0800

    modified report records to have both search bar and dropdown options, cleared up some code

--- pages/Report_Records.py
+++ pages/Report_Records.py
@@ -13,63 +13,6 @@ if check_password():
 
     # Search functionality
     st.header('Search Report by Evacuation Site')
-    # search_name = st.text_input('Enter site name:', key='original_search').strip()
-    #
-    # # DROPDOWN
-    # evac_sites_list = read_evacuation_centers()
-    # evacuation_site = st.selectbox('Select Evacuation Site', evac_sites_list)
-    #
-    #
-    #
-    #
-    # if st.button('Search'):
-    #     if search_name:
-    #         found_report = search_report_by_site(search_name)
-    #         found_inventory = search_inventory_by_site(search_name)
-    #         if found_report:
-    #             st.write(f'### {search_name.title()} Reports')
-    #             st.write(f'Total Reports Found: {len(found_report)}')
-    #             for index, report in enumerate(found_report):
-    #                 with st.expander(f"Evacuation Site Report {index + 1}: {report.evacuation_site}"):
-    #                     st.write(f"### Evacuation Site: {report.evacuation_site}")
-    #                     st.write(f"Date: {report.date}")
-    #                     st.write(f"Time: {report.time}")
-    #                     st.write('### I. Situation Overview')
-    #                     st.write(f"{report.situation}")
-    #                     st.write('### II. Status of Affected Areas and Population')
-    #                     st.write(f"{report.affected_pop}")
-    #                     st.write('### III. Status of Displaced Population')
-    #                     st.write(f"{report.displaced}")
-    #                     st.write('### IV. Response Actions and Interventions')
-    #                     st.write(f"{report.response}")
-    #                     st.write(f"Prepared by: {report.preparer}")
-    #                     st.write(f"Released by: {report.releaser}")
-    #         else:
-    #             st.warning(f"No available reports for '{search_name}'.")
-    #         if found_inventory:
-    #             st.write(f'### {search_name.title()} Inventory')
-    #             with st.expander(f"{search_name.title()} Inventory"):
-    #                 inventory_df = pd.DataFrame([
-    #                     {'Item': item.item.title(), 'Quantity': item.quantity}
-    #                     for item in found_inventory
-    #                 ])
-    #                 inventory_df["Inventory"] = inventory_df['Quantity']
-    #
-    #                 st.dataframe(inventory_df,
-    #                              width=500,
-    #                              height=420,
-    #                              column_config={
-    #                                  "Inventory": st.column_config.ProgressColumn(
-    #                                      "Inventory",
-    #                                      help="Volume in tons",
-    #                                      min_value=0,
-    #                                      max_value=100)},
-    #                              hide_index=True,
-    #                              use_container_width=True
-    #                              )
-    #
-    #         else:
-    #             st.warning(f"No inventory for '{search_name}'.")
 
     # DROPDOWN
     evac_sites_list = read_evacuation_centers()
