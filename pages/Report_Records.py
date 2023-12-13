5f66c48c7d0351b169a1aa5c8f380282c27b0db5
Mik Watkins <63586831+Mikerniker@users.noreply.github.com>
2023-12-14 00:36:51 +0800

    Deleted unnecessary sections in report_records.py

--- pages/Report_Records.py
+++ pages/Report_Records.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from datab_reports import Reports, Session
 from password_check import check_password
 from functions import read_inventory, search_inventory_by_site, \
     read_reports, search_report_by_site, read_evacuation_centers
@@ -86,39 +85,4 @@ if check_password():
                              )
         else:
             st.warning(f"No inventory for '{search_method.title()}'.")
-
-
-
-
-
-
-
-    #DELETE
-    # Display items in Streamlit for TESTING
-# reports = read_items()
-# st.write('## Items from Database')
-# for item in reports:
-#     st.write(
-#         f"**ID:** {item.id}, **Name:** {item.evacuation_site}, **Description:** {item.situation}")
-
-    # clear_search_button = st.button('Clear Search')
-    # if st.button('Clear Search'):
-    #     search_name = st.text_input('Enter site name:', key='updated_search')
-
-    # for report in reports:
-    #     st.write(f"Evacuation Site: {report.evacuation_site}")
-    #     st.write(f"Date: {report.date}")
-    #     st.write(f"Time: {report.time}")
-    #     st.write('I. Situation Overview')
-    #     st.write(f"{report.situation}")
-    #     st.write('II. Status of Affected Areas and Population')
-    #     st.write(f"{report.affected_pop}")
-    #     st.write('III. Status of Displaced Population')
-    #     st.write(f"{report.displaced}")
-    #     st.write('IV. Response Actions and Interventions')
-    #     st.write(f"{report.response}")
-    #     st.write(f"Prepared by: {report.preparer}")
-    #     st.write(f"Released by: {report.releaser}")
-
-    # session.close()
 
